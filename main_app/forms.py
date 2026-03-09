37ed1ac76d5db60944608689f1092563da46a346
MCiccone <cicconemt@gmail.com>
2026-03-09 12:34:02 -0700

    cleaned up code

--- main_app/forms.py
+++ main_app/forms.py
@@ -8,14 +8,9 @@ class OrderListItemForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.product_obj = None  # Default value
-        
-        # 1. Check initial data (used during GET request)
+        self.product_obj = None  
         product_id = self.initial.get('product')
-        
-        # 2. Check the data from the POST request
         if not product_id and self.data:
-            # Reconstruct the field name from the form prefix
             field_name = f"{self.prefix}-product"
             product_id = self.data.get(field_name)
 
@@ -24,8 +19,7 @@ class OrderListItemForm(forms.ModelForm):
                 self.product_obj = Product.objects.get(id=product_id)
             except (Product.DoesNotExist, ValueError):
                 pass
-        
-        # 3. Fallback: only try accessing instance.product if it's already in DB
+
         if not self.product_obj and self.instance.pk:
             try:
                 self.product_obj = self.instance.product
